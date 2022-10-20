35a0c13755d9b8c63c245f300f005c6f6f8949ea
Maciej Śliz-Kondratowicz <macieksk2@gmail.com>
2022-10-20 11:16:03 +0200

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -75,7 +75,8 @@ print(select_sort())
 
 ############# Bubble Sort ###################
 # DESCRIPTION
-# Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order. 
+# Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements 
+# if they are in the wrong order. 
 # This algorithm is not suitable for large data sets as its average and worst-case time complexity is quite high.
 
 # Steps:
@@ -258,6 +259,7 @@ def insertion_sort(input_ = A):
             out[j + 1] = out[j]
             j -= 1
         out[j + 1] = key
+        # print(out)
        
     return out
 
@@ -434,12 +436,512 @@ print(merge_sort())
 #As merge showed, we can merge two sorted segments in linear time, which means that each pass takes O(n) time. 
 #Since there are [log2n] passes, the total computing time is O(nlogn).
 
-#Quick Sort
+######################### Quick Sort #########################################
 
-#Heap Sort
+# DESCRIPTION
+#Like Merge Sort, QuickSort is a Divide and Conquer algorithm. 
+#It picks an element as a pivot and partitions the given array around the picked pivot. 
+#There are many different versions of quickSort that pick pivot in different ways. 
+#
+#Always pick the first element as a pivot.
+#Always pick the last element as a pivot (implemented below)
+#Pick a random element as a pivot.
+#Pick median as the pivot.
+#The key process in quickSort is a partition(). 
+#The target of partitions is, given an array and an element x of an array as the pivot,
+#put x at its correct position in a sorted array and put all smaller elements (smaller than x) before x, 
+#and put all greater elements (greater than x) after x. All this should be done in linear time.
 
-#Counting Sort
+# PYTHON IMPLEMENTATION
+A = [64, 25, 12, 22, 11, 0, 54, 21, 45, 2, 143, 1, 4, 57, 1]
 
-#Radix Sort
+def partition(input_, low, high):
+   out = input_
+   # Choose the rightmost element as pivot
+   pivot = out[high]
+   # Pointer for greater elemenet
+   i = low - 1
+   # traverse though all elemenets
+   # Compare each element with pivot
+   for j in range(low, high):
+       if out[j] <= pivot:
+           # If element smaller than pivot is found
+           # swap it with the greater element pointed by i
+           i += 1
+           # Swapping element at i with the element at j
+           (out[i], out[j]) = (out[j], out[i])
+   # swap the pivot element
+   # e greater element specified by i 
+   (out[i + 1], out[high]) = (out[high], out[i + 1])
+   # Return the position from where partition is done
+   return i + 1
+   
+def quick_sort(input_, low , high):
+    out = input_
+    if low < high:
+        # find pivot element such that
+        # elemenet smaller than pivot are on the left
+        # element greater than pivot are on the right
+        pi = partition(out, low, high)
+        # Recursive call on the left of pivot
+        quick_sort(out, low, pi - 1)
+        # Recursive call on the right of pivot
+        quick_sort(out, pi + 1, high)
+    return out
+                    
+print("Sorted array with selection sort: ")
+print(quick_sort(A, 0, len(A) - 1))
 
-#Bucket Sort
+
+# PSEUDO CODE
+
+#/* low  –> Starting index,  high  –> Ending index */
+#
+#quickSort(arr[], low, high) {
+#
+#    if (low < high) {
+#
+#        /* pi is partitioning index, arr[pi] is now at right place */
+#
+#        pi = partition(arr, low, high);
+#
+#        quickSort(arr, low, pi – 1);  // Before pi
+#
+#        quickSort(arr, pi + 1, high); // After pi
+#
+#    }
+#
+#}
+
+#Pseudo code for partition()  
+#
+#/* This function takes last element as pivot, places the pivot element at its correct position in sorted array, 
+# and places all smaller (smaller than pivot) to left of pivot and all greater elements to right of pivot */
+#
+#partition (arr[], low, high)
+#{
+#    // pivot (Element to be placed at right position)
+#    pivot = arr[high];  
+#
+#    i = (low – 1)  // Index of smaller element and indicates the 
+#    // right position of pivot found so far
+#
+#    for (j = low; j <= high- 1; j++){
+#
+#        // If current element is smaller than the pivot
+#        if (arr[j] < pivot){
+#            i++;    // increment index of smaller element
+#            swap arr[i] and arr[j]
+#        }
+#    }
+#    swap arr[i + 1] and arr[high])
+#    return (i + 1)
+#}
+
+# COMPLEXITY
+
+#Time taken by QuickSort, in general, can be written as follows. 
+#
+# T(n) = T(k) + T(n-k-1) + \theta               (n)
+#
+#The first two terms are for two recursive calls, the last term is for the partition process. 
+# k is the number of elements that are smaller than the pivot. 
+#The time taken by QuickSort depends upon the input array and partition strategy. Following are three cases.
+#
+#Worst Case: 
+#The worst case occurs when the partition process always picks the greatest or smallest element as the pivot. 
+#If we consider the above partition strategy where the last element is always picked as a pivot, 
+#the worst case would occur when the array is already sorted in increasing or decreasing order. 
+#Following is recurrence for the worst case.  
+#
+# T(n) = T(0) + T(n-1) + \theta(n)
+#which is equivalent to  T(n) = T(n-1) + \theta(n)
+#
+#The solution to the above recurrence is (n2). 
+#
+
+# STABILITY, IN-PLACE
+#
+#Is QuickSort stable? 
+#The default implementation is not stable. 
+#However any sorting algorithm can be made stable by considering indexes as comparison parameter. 
+#
+#Is QuickSort In-place? 
+#As per the broad definition of in-place algorithm it qualifies as an in-place sorting algorithm 
+#as it uses extra space only for storing recursive function calls but not for manipulating the input. 
+
+##################### Heap Sort ###########################################################################
+
+# DESCRIPTION
+
+#Heap sort is a comparison-based sorting technique based on Binary Heap data structure. 
+# It is similar to the selection sort where we first find the minimum element and place the minimum element at the beginning. 
+# Repeat the same process for the remaining elements.
+#
+#Heap sort is an in-place algorithm. 
+#Its typical implementation is not stable, but can be made stable (See this)
+#Typically 2-3 times slower than well-implemented QuickSort.  The reason for slowness is a lack of locality of reference.
+#Advantages of heapsort:
+#Efficiency –  The time required to perform Heap sort increases logarithmically 
+#while other algorithms may grow exponentially slower as the number of items to sort increases. 
+#This sorting algorithm is very efficient.
+#Memory Usage – Memory usage is minimal because apart from what is necessary to hold the initial list of items to be sorted, 
+#it needs no additional memory space to work
+#Simplicity –  It is simpler to understand than other equally efficient sorting algorithms because 
+#it does not use advanced computer science concepts such as recursion
+#Applications of HeapSort:
+#Heapsort is mainly used in hybrid algorithms like the IntroSort.
+#Sort a nearly sorted (or K sorted) array 
+#k largest(or smallest) elements in an array 
+#The heap sort algorithm has limited uses because Quicksort and Mergesort are better in practice. 
+#Nevertheless, the Heap data structure itself is enormously used. 
+
+#What is meant by Heapify? 
+#Heapify is the process of creating a heap data structure from a binary tree represented using an array. 
+# It is used to create Min-Heap or Max-heap. Start from the first index of the non-leaf node whose index is given by n/2 – 1. 
+# Heapify uses recursion
+
+# PYTHON IMPLEMENTATION
+A = [64, 25, 12, 22, 11, 0, 54, 21, 45, 2, 143, 1, 4, 57, 1]
+
+def heapify(input_, N, i):
+    # Initialize largest as root
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+    
+    # See if the left child of root exists and is
+    # greater than root
+    if l < N and input_[largest] < input_[l]:
+        largest = l
+
+    # See if the right child of root exists and is
+    # greater than root
+    if r < N and input_[largest] < input_[r]:
+        largest = r
+        
+    # Change root, if needed
+    if largest != i:
+        input_[i], input_[largest] = input_[largest], input_[i]  # swap
+ 
+        # Heapify the root.
+        heapify(input_, N, largest)
+ 
+# The main function to sort an array of given size
+def heapSort(input_):
+    out = input_
+    N = len(out)
+ 
+    # Build a maxheap.
+    for i in range(N//2 - 1, -1, -1):
+        heapify(out, N, i)
+ 
+    # One by one extract elements
+    for i in range(N-1, 0, -1):
+        out[i], out[0] = out[0], out[i]  # swap
+        heapify(out, i, 0)   
+    return out
+print("Sorted array with selection sort: ")
+print(heapSort(A))
+
+# TIME COMPLEXITY
+
+#Time Complexity: O(N log N)
+#Auxiliary Space: O(1)
+
+# STABILITY
+
+#What are the two phases of Heap Sort?
+#The heap sort algorithm consists of two phases. In the first phase the array is converted into a max heap. 
+# And in the second phase the highest element is removed (i.e., the one at the tree root) and 
+# the remaining elements are used to create a new max heap.
+#
+#Why Heap Sort is not stable?
+#Heap sort algorithm is not a stable algorithm. This algorithm is not stable because the operations 
+# that are performed in a heap can change the relative ordering of the equivalent keys.
+#
+#Is Heap Sort an example of “Divide and Conquer” algorithm?
+#Heap sort is NOT at all a Divide and Conquer algorithm. It uses a heap data structure to efficiently sort its element 
+#and not a “divide and conquer approach” to sort the elements.
+#
+#Which sorting algorithm is better – Heap sort or Merge Sort?
+#The answer lies in the comparison of their time complexity and space requirement. 
+#The Merge sort is slightly faster than the Heap sort. But on the other hand merge sort takes extra memory. Depending on the requirement, one should choose which one to use.
+#
+#Why Heap sort better than Selection sort?
+#Heap sort is similar to selection sort, but with a better way to get the maximum element. 
+# It takes advantage of the heap data structure to get the maximum element in constant time.
+
+########################### Counting Sort #############################################
+
+# DESCRIPTION
+
+
+# PYTHON IMPLEMENTATION
+
+### Counting Sort
+import random
+# Counting Sort
+def counting_sort(A):
+    # initiate
+    B = [0] * (len(A) + 1)
+    k = max(A) + 1
+    C = []
+    for i in range(k + 1):
+        C.append(0)
+    for j in range(len(A)):
+        C[A[j]] += 1
+    # C[i] now contains the number of elements equal to i
+    for i in range(k + 1):
+        C[i] += C[i - 1]
+    # C[i] now contains the number of elements less than or equal to i
+    for j in range(len(A) - 1, -1, -1):
+        B[C[A[j]]] = A[j]
+        C[A[j]] -= 1
+    # remove the first element (zero)
+    return B[1:]#, C
+
+A = [2,10,3,0,2,3,0,3]
+out = counting_sort(A)
+
+A = [2,10,3,0,2,3,0,3,76,23,52,1,0,0,5,4,7,8,3,6]
+out = counting_sort(A)
+
+A = random.sample(range(1, 1000), 100)
+out = counting_sort(A)
+
+# 8.2.3
+# Counting Sort modified
+def counting_sort_mod(A):
+    # initiate
+    B = [0] * (len(A) + 1)
+    k = max(A) + 1
+    C = []
+    for i in range(k + 1):
+        C.append(0)
+    for j in range(len(A)):
+        C[A[j]] += 1
+    # C[i] now contains the number of elements equal to i
+    for i in range(k + 1):
+        C[i] += C[i - 1]
+    # C[i] now contains the number of elements less than or equal to i
+    # Modify
+    for j in range(len(A)):
+        B[C[A[j]]] = A[j]
+        C[A[j]] -= 1
+    # remove the first element (zero)
+    return B[1:]#, C
+
+A = [2,10,3,0,2,3,0,3]
+out = counting_sort_mod(A)
+
+#A = random.sample(range(1, 1000), 100)
+#out = counting_sort_mod(A)
+
+
+####################### Radix Sort ########################################
+
+# DESCRIPTION
+
+# PYTHON IMPLEMENTATION
+
+def counting_sort(arr, exp1):
+ 
+    n = len(arr)
+    # The output array elements that will have sorted arr
+    output = [0] * (n)
+    # initialize count array as 0
+    count = [0] * (10)
+    # Store count of occurrences in count[]
+    for i in range(0, n):
+        index = arr[i] // exp1
+        count[index % 10] += 1
+    # Change count[i] so that count[i] now contains actual
+    # position of this digit in output array
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+    # Build the output array
+    i = n - 1
+    while i >= 0:
+        index = arr[i] // exp1
+        output[count[index % 10] - 1] = arr[i]
+        count[index % 10] -= 1
+        i -= 1 
+    # Copying the output array to arr[],
+    # so that arr now contains sorted numbers
+    i = 0
+    for i in range(0, len(arr)):
+        arr[i] = output[i]        
+# Method to do Radix Sort
+def radix_sort(arr):
+ 
+    # Find the maximum number to know number of digits
+    max1 = max(arr)
+ 
+    # Do counting sort for every digit. Note that instead
+    # of passing digit number, exp is passed. exp is 10^i
+    # where i is current digit number
+    exp = 1
+    while max1 / exp >= 1:
+        counting_sort(arr, exp)
+        exp *= 10
+    return arr
+
+####################### Bucket Sort ########################################
+
+# DESCRIPTION
+
+# PYTHON IMPLEMENTATION
+
+def insertionSort(b):
+    for i in range(1, len(b)):
+        up = b[i]
+        j = i - 1
+        while j >= 0 and b[j] > up: 
+            b[j + 1] = b[j]
+            j -= 1
+        b[j + 1] = up     
+    return b     
+              
+# Bucket sort for numbers 
+# having integer part
+def bucketSort(arr, noOfBuckets):
+    max_ele = max(arr)
+    min_ele = min(arr)
+  
+    # range(for buckets)
+    rnge = (max_ele - min_ele) / noOfBuckets
+    temp = []
+  
+    # create empty buckets
+    for i in range(noOfBuckets):
+        temp.append([])
+  
+    # scatter the array elements
+    # into the correct bucket
+    for i in range(len(arr)):
+        diff = (arr[i] - min_ele) / rnge - int((arr[i] - min_ele) / rnge)
+  
+        # append the boundary elements to the lower array
+        if(diff == 0 and arr[i] != min_ele):
+            temp[int((arr[i] - min_ele) / rnge) - 1].append(arr[i])
+  
+        else:
+            temp[int((arr[i] - min_ele) / rnge)].append(arr[i])
+  
+    # Sort each bucket individually
+    for i in range(len(temp)):
+        if len(temp[i]) != 0:
+            temp[i].sort()
+  
+    # Gather sorted elements 
+    # to the original array
+    k = 0
+    for lst in temp:
+        if lst:
+            for i in lst:
+                arr[k] = i
+                k = k+1
+    return temp
+
+# Driver Code
+A = [0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434] 
+print("Sorted Array is")
+print(bucketSort(A))
+
+# test efficiency
+import random
+import time
+from matplotlib import pyplot as plt
+
+def time_elapsed(func, inp_range = 10000, inp_no = 1000):
+    random.seed(10)
+    A = random.sample(range(1, inp_range), inp_no)
+    start = time.time()
+    print(str(func))
+    if func == "quick_sort":
+        test = eval(str(func) + "(A,0, len(A) - 1)")
+    elif func == "bucket_sort":
+        test = eval(str(func) + "(A,1)")
+    else:
+        test = eval(str(func) + "(A)")
+    end = time.time()
+    print(end - start)
+    return end - start
+    
+test1 = time_elapsed("select_sort")
+test2 = time_elapsed("bubble_sort")
+test3 = time_elapsed("bubble_sort_optim")
+test4 = time_elapsed("insertion_sort")
+test5 = time_elapsed("merge_sort")
+test6 = time_elapsed("quick_sort")
+test7 = time_elapsed("heapSort")
+test8 = time_elapsed("radix_sort")
+test9 = time_elapsed("bucketSort")
+
+
+# test efficiency for different lengths
+test_len_1 = []
+for n in range(999):
+    print(n)
+    test1 = time_elapsed("select_sort", inp_no = n + 1)
+    test_len_1.append(test1)
+    
+plt.plot(test_len_1)
+plt.show()
+
+test_len_2 = []
+for n in range(999):
+    print(n)
+    test2 = time_elapsed("bubble_sort", inp_no = n + 1)
+    test_len_2.append(test2)
+    
+plt.plot(test_len_2)
+plt.show()
+
+test_len_3 = []
+for n in range(999):
+    print(n)
+    test3 = time_elapsed("bubble_sort_optim", inp_no = n + 1)
+    test_len_3.append(test3)
+    
+plt.plot(test_len_3)
+plt.show()
+
+test2_add = time_elapsed("bubble_sort", inp_range = 100000, inp_no = 50000)
+test3_add = time_elapsed("bubble_sort_optim", inp_range = 100000, inp_no = 50000)
+
+
+test_len_4 = []
+for n in range(999):
+    print(n)
+    test4 = time_elapsed("insertion_sort", inp_no = n + 1)
+    test_len_4.append(test4)
+    
+plt.plot(test_len_4)
+plt.show()
+
+test_len_5 = []
+for n in range(9999):
+    print(n)
+    test5 = time_elapsed("merge_sort", inp_no = n + 1)
+    test_len_5.append(test5)
+    
+plt.plot(test_len_5)
+plt.show()
+
+test_len_6 = []
+for n in range(4999):
+    print(n)
+    test6 = time_elapsed("quick_sort", inp_no = n + 1)
+    test_len_6.append(test6)
+    
+plt.plot(test_len_6)
+plt.show()
+
+plt.plot([x / test_len_6[2] for x in test_len_6])
+plt.show()
+
+plt.plot([x * np.log(x) for x in range(len(test_len_6))])
+plt.show()
+
